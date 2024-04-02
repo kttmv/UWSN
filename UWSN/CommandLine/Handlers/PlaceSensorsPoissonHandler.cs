2b4cf1e540ee8ea5d6702466012edbd8ef4f9337
Razmes <ruslanmail134@mail.ru>
2024-04-02 20:12:00 +0300

    Псевдослучайное распределение

--- UWSN/CommandLine/Handlers/PlaceSensorsPoissonHandler.cs
+++ UWSN/CommandLine/Handlers/PlaceSensorsPoissonHandler.cs
@@ -26,11 +26,48 @@ public class PlaceSensorsPoissonHandler
 
         var areaLimits = Simulation.Instance.AreaLimits;
 
-        Simulation.Instance.Environment.Sensors = PlaceSensors(sensors, o.LambdaParameter, areaLimits);
+        Simulation.Instance.Environment.Sensors = PlaceSensors(sensors, areaLimits);
 
         Console.WriteLine($"Расстановка сенсоров ({o.SensorsCount}) по закону Пуассона прошла успешно.");
 
         SerializationHelper.SaveSimulation(o.FilePath);
+    }
+
+    private static List<Sensor> PlaceSensors(List<Sensor> sensors, Vector3Range areaLimits)
+    {
+        int d = 3;
+        int n = 50;
+        double seed = 100.0;
+        double g = Phi(d);
+        double[] alpha = new double[d];
+        double[,] R = new double[n, d];
+
+        for (int j = 0; j < d; j++)
+        {
+            alpha[j] = Math.Pow(1 / g, j + 1);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < d; j++)
+            {
+                R[i, j] = (seed + alpha[j] * (i + 1));
+            }
+        }
+
+        R = MinMaxStandardize(R);
+
+        for (int i = 0; i < sensors.Count; i++)
+        {
+            float x = (float)PoissonDouble(R[i, 0], areaLimits.Min.X, areaLimits.Max.X);
+            float y = (float)PoissonDouble(R[i, 1], areaLimits.Min.Y, areaLimits.Max.Y);
+            float z = (float)PoissonDouble(R[i, 2], areaLimits.Min.Z, areaLimits.Max.Z);
+
+            sensors[i].Position = new Vector3(x, y, z);
+        }
+
+        return sensors;
+
     }
 
     private static List<Sensor> PlaceSensors(List<Sensor> sensors, double lambdaParameter, Vector3Range areaLimits)
@@ -71,5 +108,47 @@ public class PlaceSensorsPoissonHandler
     private static double PoissonDouble(double uniformValue, double min, double max)
     {
         return min + (uniformValue * (max - min));
+    }
+
+    private static double Phi(int d)
+    {
+        double x = 2.0000;
+        for (int i = 0; i < 10; i++)
+        {
+            x = Math.Pow(1 + x, 1 / (d + 1.0));
+        }
+        return x;
+    }
+
+    /// <summary>
+    /// Алгоритм минимаксной стандартизации от 0 до 1 двумерного массива координат сенсоров
+    /// </summary>
+    /// <param name="R">Массив координат</param>
+    /// <returns></returns>
+    private static double[,] MinMaxStandardize(double[,] R)
+    {
+        double min = double.MaxValue;
+        double max = double.MinValue;
+
+        // Находим минимальное и максимальное значения в массиве R
+        for (int i = 0; i < R.GetLength(0); i++)
+        {
+            for (int j = 0; j < R.GetLength(1); j++)
+            {
+                min = Math.Min(min, R[i, j]);
+                max = Math.Max(max, R[i, j]);
+            }
+        }
+
+        // Производим стандартизацию чисел от 0 до 1
+        for (int i = 0; i < R.GetLength(0); i++)
+        {
+            for (int j = 0; j < R.GetLength(1); j++)
+            {
+                R[i, j] = (R[i, j] - min) / (max - min);
+            }
+        }
+
+        return R;
     }
 }
