c7e943161b0362ed50af28e8c5ee50db4df2d434
kttmv <ktototamovich20@gmail.com>
2024-04-29 00:13:37 +0300

    front: редактирование границ

--- client/src/renderer/store/projectStore.ts
+++ client/src/renderer/store/projectStore.ts
@@ -1,14 +1,15 @@
 import { create } from 'zustand'
-import { readFile } from '../shared/helpers/fsHelpers'
+import { readFile, writeFile } from '../shared/helpers/fsHelpers'
 import { ProjectData } from '../shared/types/projectData'
 
 type State = {
     projectFilePath: string
     setProjectFilePath: (value: string) => void
     project: ProjectData | undefined
+    setProject: (value: ProjectData) => void
 }
 
-const useProjectStore = create<State>((set) => ({
+const useProjectStore = create<State>((set, get) => ({
     projectFilePath: '',
     project: undefined,
     setProjectFilePath: async (path: string) => {
@@ -22,6 +23,20 @@ const useProjectStore = create<State>((set) => ({
         } catch (error) {
             // todo: показывать ошибку в модальном окне
             console.error(`Не удалось прочитать файл ${path}.`, error)
+        }
+    },
+    setProject: async (newProject: ProjectData) => {
+        const path = get().projectFilePath
+
+        try {
+            writeFile(path, JSON.stringify(newProject, null, 4))
+
+            set(() => ({ project: newProject }))
+        } catch (error) {
+            console.error(
+                `Не удалось записать изменения в файл ${path}.`,
+                error
+            )
         }
     }
 }))
--- client/src/renderer/tabs/EnvironmentBoundaries.tsx
+++ client/src/renderer/tabs/EnvironmentBoundaries.tsx
@@ -7,81 +7,97 @@ import {
     Input,
     Text
 } from '@chakra-ui/react'
-import { FieldErrors, UseFormRegister } from 'react-hook-form'
+import { FormState, UseFormRegister } from 'react-hook-form'
 import { EnvironmentInputs } from './EnvironmentTab'
 
 interface Props {
     register: UseFormRegister<EnvironmentInputs>
-    errors: FieldErrors<EnvironmentInputs>
+    formState: FormState<EnvironmentInputs>
 }
 
-export default function EnvironmentBoundaries({ register, errors }: Props) {
+export default function EnvironmentBoundaries({ register, formState }: Props) {
+    const errors = formState.errors
+    const dirtyFields = formState.dirtyFields
+
     return (
         <>
             <FormControl isInvalid={Object.keys(errors).length !== 0}>
                 <FormLabel>Границы</FormLabel>
+
                 <Grid templateColumns='repeat(3, 1fr)' gap={4}>
                     <Flex align='center' gap={2}>
                         <Text>x₁</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v1?.x?.type === 'required'}
-                            {...register('v1.x', {
+                            isInvalid={errors.v1?.X?.type === 'required'}
+                            fontWeight={dirtyFields.v1?.X ? 'bold' : undefined}
+                            {...register('v1.X', {
                                 required: 'Отсутствует значение параметра x₁'
                             })}
                         />
                     </Flex>
+
                     <Flex align='center' gap={2}>
                         <Text>y₁</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v1?.y?.type === 'required'}
-                            {...register('v1.y', {
+                            isInvalid={errors.v1?.Y?.type === 'required'}
+                            fontWeight={dirtyFields.v1?.Y ? 'bold' : undefined}
+                            {...register('v1.Y', {
                                 required: 'Отсутствует значение параметра y₁'
                             })}
                         />
                     </Flex>
+
                     <Flex align='center' gap={2}>
                         <Text>z₁</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v1?.z?.type === 'required'}
-                            {...register('v1.z', {
+                            isInvalid={errors.v1?.Z?.type === 'required'}
+                            fontWeight={dirtyFields.v1?.Z ? 'bold' : undefined}
+                            {...register('v1.Z', {
                                 required: 'Отсутствует значение параметра z₁'
                             })}
                         />
                     </Flex>
+
                     <Flex align='center' gap={2}>
                         <Text>x₂</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v2?.x?.type === 'required'}
-                            {...register('v2.x', {
+                            isInvalid={errors.v2?.X?.type === 'required'}
+                            fontWeight={dirtyFields.v2?.X ? 'bold' : undefined}
+                            {...register('v2.X', {
                                 required: 'Отсутствует значение параметра x₂'
                             })}
                         />
                     </Flex>
+
                     <Flex align='center' gap={2}>
                         <Text>y₂</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v2?.y?.type === 'required'}
-                            {...register('v2.y', {
+                            isInvalid={errors.v2?.Y?.type === 'required'}
+                            fontWeight={dirtyFields.v2?.Y ? 'bold' : undefined}
+                            {...register('v2.Y', {
                                 required: 'Отсутствует значение параметра y₂'
                             })}
                         />
                     </Flex>
+
                     <Flex align='center' gap={2}>
                         <Text>z₂</Text>
                         <Input
                             type='number'
-                            isInvalid={errors.v2?.z?.type === 'required'}
-                            {...register('v2.z', {
+                            isInvalid={errors.v2?.Z?.type === 'required'}
+                            fontWeight={dirtyFields.v2?.Z ? 'bold' : undefined}
+                            {...register('v2.Z', {
                                 required: 'Отсутствует значение параметра z₂'
                             })}
                         />
                     </Flex>
                 </Grid>
+
                 <FormErrorMessage>
                     Необходимо ввести все значения
                 </FormErrorMessage>
