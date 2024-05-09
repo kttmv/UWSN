e94d20c97fa8a0c552f613d5633c9ba03f928c4e
Vlad <ktototamovich20@gmail.com>
2024-05-09 18:04:04 +0300

    sim: сохранение вывода консоли в файл output.txt

--- UWSN/Utilities/Logger.cs
+++ UWSN/Utilities/Logger.cs
@@ -5,6 +5,8 @@ namespace UWSN.Utilities;
 
 public class Logger
 {
+    private static List<string> Lines { get; set; } = new();
+
     public static void WriteLine(string value, bool withTime = false)
     {
         string str = withTime ? $"[{Simulation.Instance.Time:dd.MM.yyyy HH:mm:ss.fff}] " : "";
@@ -12,10 +14,24 @@ public class Logger
         str += value;
 
         Console.WriteLine(str);
+        Lines.Add(str);
     }
 
     public static void WriteSensorLine(Sensor sensor, string value)
     {
         WriteLine($"Сенсор #{sensor.Id}: {value}");
+    }
+
+    public static void Save()
+    {
+        string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        string strWorkPath = System.IO.Path.GetDirectoryName(strExeFilePath)!;
+        string path = Path.Combine(strWorkPath, "output.txt");
+
+        var content = string.Join("\n", Lines.ToArray());
+
+        File.WriteAllText(path, content);
+
+        Console.WriteLine($"Вывод программы сохранен в {path}");
     }
 }
