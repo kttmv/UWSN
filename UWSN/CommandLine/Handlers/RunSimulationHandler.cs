b6a0e6e249d66ce7f24b6b2c035571f85d83494c
kttmv <ktototamovich20@gmail.com>
2024-05-09 15:53:30 +0300

    sim: фикс сохранения дельты кластеризации при пропуске hello

--- UWSN/CommandLine/Handlers/RunSimulationHandler.cs
+++ UWSN/CommandLine/Handlers/RunSimulationHandler.cs
@@ -10,11 +10,6 @@ namespace UWSN.CommandLine.Handlers
         {
             SerializationHelper.LoadSimulation(o.FilePath);
 
-            foreach (var sensor in Simulation.Instance.Environment.Sensors)
-            {
-                sensor.WakeUp();
-            }
-
             Simulation.Instance.Run();
 
             SerializationHelper.SaveSimulation(o.FilePath);
